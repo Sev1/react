902f698dbb0b3d6dcdd7fe6c6daf5693bbd276dd
liucf <liucf@zjlantone.com>
2019-08-27 17:42:32 +0800

    放开依赖eject

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './Components/List.jsx'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <List />
       </header>
     </div>
   );
---
+++ src/Components/List.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './List.less';
+
+class List extends React.Component{
+  render(){
+    return <div className="wrap">
+      <ul className="box">
+        <li>就是测试啊</li>
+        <li>就是测试啊</li>
+        <li>就是测试啊</li>
+        <li>就是测试啊</li>
+        <li>就是测试啊</li>
+      </ul>
+    </div>
+  }
+}
+
+export default List;
